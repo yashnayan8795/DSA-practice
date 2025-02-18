b0a8fd0847685ee945f74a4a8b7bfb6d1534d341
Yash Nayan <115628084+yashnayan8795@users.noreply.github.com>
2025-02-18 19:38:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (49.49%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
@@ -5,7 +5,7 @@ public:
         // string num = "";
         // for (int i = 1; i <= n + 1; i++) {
         //     num.push_back(i + '0'); 
-        // }
+        // }       //brute force
         // while (true) {
         //     bool isValid = true;
         //     for (int i=0;i<n;i++) {
@@ -23,7 +23,7 @@ public:
         string result="";
         int n = pattern.size()+1;
         for(int i=1;i<=n;i++){
-            st.push(i+'0');
+            st.push(i+'0');       // stack ops
             if(i==n||pattern [i-1]=='I'){
                 while(!st.empty()){
                     result +=st.top();
