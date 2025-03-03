9a22831bb63e147287a6aef2290d35b0346f46c8
Yash Nayan <115628084+yashnayan8795@users.noreply.github.com>
2025-03-03 22:35:21 +0530

    Time: 11 ms (31.49%) | Memory: 127.8 MB (71.24%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,24 +1,55 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int> smaller;
-        vector<int> equal;
-        vector<int> larger;
+        // vector<int> smaller;
+        // vector<int> equal;
+        // vector<int> larger;
 
         int n=nums.size();
 
-        for(int i=0;i<n;i++){
-            if(nums[i]<pivot)
-                smaller.push_back(nums[i]);
-            else if(nums[i]==pivot)
-                equal.push_back(nums[i]);
-            else
-                larger.push_back(nums[i]);
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]<pivot)
+        //         smaller.push_back(nums[i]);
+        //     else if(nums[i]==pivot)
+        //         equal.push_back(nums[i]);
+        //     else
+        //         larger.push_back(nums[i]);
+        // }
+
+        // smaller.insert(smaller.end(), equal.begin(), equal.end());
+        // smaller.insert(smaller.end(), larger.begin(), larger.end());
+
+        // return smaller;
+
+        int smaller = 0;
+        int equal = 0;
+
+        for(int &num : nums) {
+            if(num < pivot)
+                smaller++;
+            else if(num == pivot)
+                equal++;
         }
 
-        smaller.insert(smaller.end(), equal.begin(), equal.end());
-        smaller.insert(smaller.end(), larger.begin(), larger.end());
+        int i = 0;
+        int j = smaller;
+        int k = smaller + equal;
+        vector<int> result(n);
 
-        return smaller;
+        for(int &num : nums) {
+            if(num < pivot) {
+                result[i] = num;
+                i++;
+            } else if(num == pivot) {
+                result[j] = num;
+                j++;
+            } else {
+                result[k] = num;
+                k++;
+            }
+        }
+        
+        return result;
+
     }
 };
