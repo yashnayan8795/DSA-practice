04bc666cc439f9f066513e45b089e639eb15b3eb
Yash Nayan <115628084+yashnayan8795@users.noreply.github.com>
2025-03-04 16:35:51 +0530

    Time: 7 ms (28.57%) | Memory: 16.7 MB (99.83%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,39 +1,56 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-         vector<vector<pair<int, int>>> grid(n); // Properly initialize grid
+    //      vector<vector<pair<int, int>>> grid(n); 
         
-        for(auto i:flights)
-            grid[i[0]].push_back({i[1],i[2]});
+    //     for(auto i:flights)
+    //         grid[i[0]].push_back({i[1],i[2]});
 
-        queue<pair<int,pair<int,int>>> q;
-        q.push({0,{src,0}});
+    //     queue<pair<int,pair<int,int>>> q;
+    //     q.push({0,{src,0}});
 
-        vector<int> dist(n,1e9);
-        dist[src]=0;
-        while(!q.empty()){
-            auto it=q.front();
-            q.pop();
+    //     vector<int> dist(n,1e9);
+    //     dist[src]=0;
+    //     while(!q.empty()){
+    //         auto it=q.front();
+    //         q.pop();
 
-            int stop=it.first;
-            int node= it.second.first;
-            int cost= it.second.second;
+    //         int stop=it.first;
+    //         int node= it.second.first;
+    //         int cost= it.second.second;
 
-            if(stop>k) continue;
-            for (auto j : grid[node])
-            {
-                int gridnode = j.first;
-                int edges_w = j.second;
-                if (cost + edges_w < dist[gridnode] && stop <= k)
-                {
-                    dist[gridnode] = cost + edges_w;
-                    q.push({stop + 1, {gridnode, cost + edges_w}});
+    //         if(stop>k) continue;
+    //         for (auto j : grid[node])
+    //         {
+    //             int gridnode = j.first;
+    //             int edges_w = j.second;
+    //             if (cost + edges_w < dist[gridnode] && stop <= k)
+    //             {
+    //                 dist[gridnode] = cost + edges_w;
+    //                 q.push({stop + 1, {gridnode, cost + edges_w}});
+    //             }
+    //         }
+    //     }
+    //     if (dist[dst] == 1e9)
+    //         return -1;
+    //     return dist[dst];
+    // }
+        int INF = 0x3f3f3f3f; 
+        vector<int> distances(n, INF);
+        vector<int> previousIterationDistances; 
+        distances[src] = 0; 
+        for (int i = 0; i < k + 1; ++i) {
+            
+            previousIterationDistances = distances;
+
+            for (const auto& flight : flights) {
+                int from = flight[0], to = flight[1], price = flight[2];
+              
+                if (previousIterationDistances[from] < INF) {
+                    distances[to] = min(distances[to], previousIterationDistances[from] + price);
                 }
             }
         }
-        if (dist[dst] == 1e9)
-            return -1;
-        return dist[dst];
+        return distances[dst] == INF ? -1 : distances[dst];
     }
-    
 };      
