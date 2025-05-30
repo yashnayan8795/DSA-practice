f17df05017ad6656d8c78ef97bc402d49c7e621c
Yash Nayan <115628084+yashnayan8795@users.noreply.github.com>
2025-05-30 14:10:01 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (17.11%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -11,19 +11,15 @@
  */
 class Solution {
  public:
-  vector<int> postorderTraversal(TreeNode* root) {
-    vector<int> ans;
-    postorder(root, ans);
-    return ans;
-  }
-
- private:
-  void postorder(TreeNode* root, vector<int>& ans) {
-    if (root == nullptr)
-      return;
-
-    postorder(root->left, ans);
-    postorder(root->right, ans);
-    ans.push_back(root->val);
-  }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        postorder(root, ans);
+        return ans;
+    }
+    void postorder(TreeNode* root, vector<int>& ans) {
+        if (root == NULL) return;
+        postorder(root->left, ans);
+        postorder(root->right, ans);
+        ans.push_back(root->val);
+    }
 };
