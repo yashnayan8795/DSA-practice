ca9280a9379053e656bdc7da8423f264f82f97d8
Yash Nayan <115628084+yashnayan8795@users.noreply.github.com>
2025-06-03 23:10:25 +0530

    Time: 36 ms (7.09%) | Memory: 11 MB (42.25%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int ans = 0;
+        int n = s.size();
+        for(char c = 'A'; c <= 'Z'; c++)
+        {
+            int i = 0;
+            int j = 0;
+            int r = 0;
+            while(j < n)
+            {
+                if(s[j] == c)
+                    j++;
+                else if(r < k)
+                    j++, r++;
+                else if(s[i] == c)
+                    i++;
+                else
+                    i++, r--;
+                ans = max(ans, j - i);
+            }
+        }
+        return ans;
+    }
+};
