08f3968e50f1e47ac53526d302d0976d5c9886f9
Yash Nayan <115628084+yashnayan8795@users.noreply.github.com>
2025-06-06 09:54:16 +0530

    Time: 6 ms (45.53%) | Memory: 42.7 MB (98.96%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -9,8 +9,8 @@ class Solution {
             alphabets[t.charAt(i)-'a']--;
 
         }
-        for (int i = 0; i < 26; ++i) {
-            if (alphabets[i] != 0) return false;
+        for (int count : alphabets) {
+            if (count != 0) return false;
         }
 
         return true;
