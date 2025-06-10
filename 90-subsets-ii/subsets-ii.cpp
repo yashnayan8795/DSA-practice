7f8e8a7e197e2cbcf3be91e4ebcb95d6053abd5f
Yash Nayan <115628084+yashnayan8795@users.noreply.github.com>
2025-06-10 20:35:53 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (85.56%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> allsubsets;
+        vector<int> ans;
+        ps(nums, ans, 0, allsubsets);
+        return allsubsets;
+    }
+    
+    void ps(vector<int>& nums, vector<int>& ans, int i, vector<vector<int>>& allsubsets) {
+        if(i == nums.size()) {
+            allsubsets.push_back(ans);  
+            return;
+        }
+        
+        ans.push_back(nums[i]);
+        ps(nums, ans, i+1, allsubsets);
+        
+        ans.pop_back();
+        int idx=i+1;
+        while(idx<nums.size() && nums[i]==nums[idx]){
+            idx++;
+        }
+        ps(nums,ans,idx,allsubsets);
+    }
+};
