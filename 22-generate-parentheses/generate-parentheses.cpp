d08d37f0d6d02c0083a7c7674717b089d9d4cd6b
Yash Nayan <115628084+yashnayan8795@users.noreply.github.com>
2025-06-16 09:49:08 +0530

    Time: 6 ms (16.04%) | Memory: 16.3 MB (22.05%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
-    vector<string> generateParenthesis(int n) {
-        vector<string> result;
-        string curr = "";
-        solve(result, curr, n);
-        return result;
-    }
-
-    void solve(vector<string>& result, string curr, int n) {
-        if (curr.length() == 2 * n) {
-            if (isValid(curr)) {
-                result.push_back(curr);
-            }
+    void solve(vector<string>& result,int n, string curr, int open, int close) {
+        if(curr.length() == 2*n) {
+            result.push_back(curr);
             return;
         }
-
-        solve(result, curr + "(", n);
-        solve(result, curr + ")", n);
-    }
-
-    bool isValid(string str) {
-        int counter = 0;
-        for (char c : str) {
-            if (c == '(') counter++;
-            else if (c == ')') counter--;
-            if (counter < 0) return false; // more ')' than '('
+        
+        if(open < n) {
+            curr.push_back('(');
+            solve(result,n, curr, open+1, close);
+            curr.pop_back();
         }
-        return counter == 0; // must be balanced
+        if(close < open) {
+            curr.push_back(')');
+            solve( result,n, curr, open, close+1);
+            curr.pop_back();
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        string curr = "";
+        vector<string> result;
+
+
+        solve(result,n, curr, 0, 0);
+
+        return result;
     }
 };
