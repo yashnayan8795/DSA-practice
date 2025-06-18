06a4714bef8de61b7a536ed68c67eb8f399f95bc
Yash Nayan <115628084+yashnayan8795@users.noreply.github.com>
2025-06-18 15:27:45 +0530

    Time: 47 ms (16.45%) | Memory: 92.1 MB (28.76%) - LeetSync

---
+++ 937-online-stock-span/online-stock-span.cpp
@@ -0,0 +1,19 @@
+class StockSpanner {
+private:
+    stack<pair<int, int>> s; 
+
+public:
+    
+    int next(int price) {
+        int span = 1;
+
+        
+        while (!s.empty() && s.top().first <= price) {
+            span += s.top().second;
+            s.pop();
+        }
+
+        s.push({price, span});
+        return span;
+    }
+};
