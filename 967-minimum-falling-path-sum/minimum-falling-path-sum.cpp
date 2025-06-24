cb4122b19fb07ad67eff2bcd7d53827936c13b4e
Yash Nayan <115628084+yashnayan8795@users.noreply.github.com>
2025-06-24 19:47:00 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (76.06%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -3,18 +3,19 @@ public:
 
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<vector<int>> dp = matrix; // Start from base case
+        vector<int> nextRow = matrix[n - 1];
 
         for (int i = n - 2; i >= 0; i--) {
+            vector<int> curRow(n);
             for (int j = 0; j < n; j++) {
-                int down = dp[i + 1][j];
-                int left = (j > 0) ? dp[i + 1][j - 1] : 1e9;
-                int right = (j < n - 1) ? dp[i + 1][j + 1] : 1e9;
-
-                dp[i][j] = matrix[i][j] + min({down, left, right});
+                int down = nextRow[j];
+                int left = (j > 0) ? nextRow[j - 1] : 1e9;
+                int right = (j < n - 1) ? nextRow[j + 1] : 1e9;
+                curRow[j] = matrix[i][j] + min({down, left, right});
             }
+            nextRow = curRow;
         }
 
-        return *min_element(dp[0].begin(), dp[0].end());
+        return *min_element(nextRow.begin(), nextRow.end());
     }
 };
