c49864c1d24ae6919fdb13016e83be30b5554cc6
Yash Nayan <115628084+yashnayan8795@users.noreply.github.com>
2025-07-22 00:26:05 +0530

    Time: 52 ms (73.57%) | Memory: 125.1 MB (43.62%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/find-median-from-data-stream.cpp
@@ -1,32 +1,38 @@
 class MedianFinder {
 public:
-    priority_queue<int> left_max_heap; //max heap
-    priority_queue<int, vector<int>, greater<int>> right_min_heap; //min heap
+    priority_queue<int> maxheap;
+    priority_queue<int,vector<int>, greater<int>> minheap;
     MedianFinder() {
         
     }
     
     void addNum(int num) {
-        if(left_max_heap.empty() || num < left_max_heap.top()) {
-            left_max_heap.push(num);
+        if(maxheap.empty()||num < maxheap.top()) {
+            maxheap.push(num);
         } else {
-            right_min_heap.push(num);
+            minheap.push(num);
         }
-        if(abs((int)left_max_heap.size() - (int)right_min_heap.size()) > 1) {
-            right_min_heap.push(left_max_heap.top());
-            left_max_heap.pop();
-        } else if(left_max_heap.size() < right_min_heap.size()) {
-            left_max_heap.push(right_min_heap.top());
-            right_min_heap.pop();
+
+        if(maxheap.size() > minheap.size() +1) {
+            minheap.push(maxheap.top());
+            maxheap.pop();
+        } else if(minheap.size() > maxheap.size()) {
+            maxheap.push(minheap.top()); 
+            minheap.pop();
         }
-        
     }
     
     double findMedian() {
-        if(left_max_heap.size() == right_min_heap.size()) {
-            
-            return (double)(left_max_heap.top()+right_min_heap.top())/2;
+        if(maxheap.size() == minheap.size()){
+            return (maxheap.top()+ minheap.top())/2.0;
         }
-        return left_max_heap.top();
+        return maxheap.top();
     }
 };
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
