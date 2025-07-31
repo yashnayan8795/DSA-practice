d69b7947d0dab0fd0d089d1db74a8c9f72824ee7
Yash Nayan <115628084+yashnayan8795@users.noreply.github.com>
2025-07-31 11:40:49 +0530

    Time: 8 ms (59.96%) | Memory: 22.9 MB (42.16%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -15,21 +15,18 @@ public:
     }
     
     int minEatingSpeed(vector<int>& piles, int h) {
-        int n = piles.size();
         
-        int l = 1, r = *max_element(begin(piles), end(piles));
-        
-        while(l < r) {
-            int mid = l + (r-l)/2;
-            
-            if(canEatAll(piles, mid, h)) {
-                r = mid;
-            } else {
-                l = mid+1;
+        int n= piles.size();
+        int l= 1;
+        int r= *max_element(begin(piles), end(piles));
+        while(l<r){
+            int mid= l+(r-l)/2;
+            if(canEatAll(piles,mid,h)){
+                r= mid;
+            }else{
+                l= mid+1;
             }
-            
         }
-        
         return l;
     }
 };
