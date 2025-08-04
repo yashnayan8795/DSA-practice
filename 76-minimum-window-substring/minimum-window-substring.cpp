e5a72a80108214b7ec4620d41c6841c644c1ece6
Yash Nayan <115628084+yashnayan8795@users.noreply.github.com>
2025-08-05 02:18:27 +0530

    Time: 2 ms (85.56%) | Memory: 11.4 MB (84.42%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -3,8 +3,8 @@ public:
     string minWindow(string& s, string& t) {
         if(t.size() > s.size()) return "";
 
-        unordered_map<char, int> wordMap;
-        unordered_map<char, int> windowMap;
+        vector<int> wordMap(256);
+        vector<int> windowMap(256);
         int total = t.size();
         int count = 0;
 
@@ -17,7 +17,7 @@ public:
         int low = 0;
         for(int r=0; r<s.size(); r++){
             char c = s[r];
-            if(wordMap.count(c)){
+            if(wordMap[c] > 0){
                 windowMap[c]++;
                 if(windowMap[c] <= wordMap[c]) count++;
             }
@@ -28,7 +28,7 @@ public:
                 }
 
                 char lc = s[l];
-                if(wordMap.count(lc)){
+                if(wordMap[lc] > 0 ){
                     windowMap[lc]--;
                     if(windowMap[lc] < wordMap[lc]) count--;
                 }
